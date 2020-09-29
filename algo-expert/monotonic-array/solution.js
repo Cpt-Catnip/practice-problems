ceb0705324a431bb51e96d62614fd8bd5c0dc4cd
Mike Koshakow <mzkosh@gmail.com>
2020-09-29 11:27:12 -0400

    solution for monotonic array

--- algo-expert/monotonic-array/solution.js
+++ algo-expert/monotonic-array/solution.js
@@ -1,3 +1,18 @@
-function isMonotonic(arr) {}
+function isMonotonic(array) {
+  if (array.length < 2) return true;
+
+  let isNonIncreasing = true;
+  let isNonDecreasing = true;
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > array[i - 1]) {
+      isNonIncreasing = false;
+    } else if (array[i] < array[i - 1]) {
+      isNonDecreasing = false;
+    }
+  }
+
+  return isNonIncreasing || isNonDecreasing;
+}
 
 module.exports = isMonotonic;
--- algo-expert/monotonic-array/tests.js
+++ algo-expert/monotonic-array/tests.js
@@ -1,8 +1,14 @@
 const { expect } = require('chai');
 const isMonotonic = require('./solution');
 
-it('[-1, -5, -10, -1100, -1100, -1101, -1102, -9001]', () => {
-  expect(
-    isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001])
-  ).to.equal(true);
+describe('Monotonic Array', () => {
+  it('Test Case 1', () => {
+    expect(
+      isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001])
+    ).to.equal(true);
+  });
+
+  it('Test Case 2', () => {
+    expect(isMonotonic([])).to.equal(true);
+  });
 });
